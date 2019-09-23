ecf26c1579fbf4ea0d0e9fa0ca01766aad0df326
leejinseok <sonaky47@gmail.com>
2019-09-23 10:08:08 +0900

    퀵정렬 커서이름변경

--- sort/quick-sort.js
+++ sort/quick-sort.js
@@ -6,30 +6,30 @@ function quickSort(array) {
 
   const pivot = array[0];
 
-  let leftCursor = 1;
-  let rightCursor = array.length - 1;
-  while(leftCursor <= rightCursor) {
+  let lowCursor = 1;
+  let highCursor = array.length - 1;
+  while(lowCursor <= highCursor) {
 
     // 왼쪽수가 기준보다 크고 오른쪽 수가 기준보다 작으면 위치를 바꿉니다 (swap)
-    if (array[leftCursor] > pivot && array[rightCursor] < pivot) {
-      [array[leftCursor], array[rightCursor]] = [array[rightCursor], array[leftCursor]];
-      leftCursor++;
-      rightCursor--;
+    if (array[lowCursor] > pivot && array[highCursor] < pivot) {
+      [array[lowCursor], array[highCursor]] = [array[highCursor], array[lowCursor]];
+      lowCursor++;
+      highCursor--;
     }
 
     // 왼쪽 수는 기준보다 작으면 다음으로 넘어가고, 크면 가만히 있습니다
-    if (array[leftCursor] <= pivot) {
-      leftCursor++;
+    if (array[lowCursor] <= pivot) {
+      lowCursor++;
     }
 
     // 오른쪽 수는 기준보다 크면 다음으로 넘어가고, 작으면 가만히
-    if (array[rightCursor] >= pivot) {
-      rightCursor--;
+    if (array[highCursor] >= pivot) {
+      highCursor--;
     }
   }
 
-  [array[0], array[leftCursor-1]] = [array[leftCursor-1], array[0]];
-  const left = array.splice(0, leftCursor-1);
+  [array[0], array[lowCursor-1]] = [array[lowCursor-1], array[0]];
+  const left = array.splice(0, lowCursor-1);
   const mid = array.splice(0, 1);
   const right = array;
 
