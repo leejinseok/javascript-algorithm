ccd4211b1c1b3898b4d4adc77493c7fb6f2b69f0
leejinseok <sonaky47@gmail.com>
2019-09-23 18:17:04 +0900

    update

--- data-structure/linked-list.js
+++ data-structure/linked-list.js
@@ -12,6 +12,7 @@ LinkedList.prototype.append = function(data) {
   const node = new Node(data);
   if (this.head === null) {
       this.head = node;
+      return node;
   } else {
     let cur = this.head;
     while(cur.next) {
@@ -21,20 +22,21 @@ LinkedList.prototype.append = function(data) {
   }
 
   this.length++;
+  return node;
 }
 
-LinkedList.prototype.removeAt = function(index) {
-  if (index > this.length - 1) {
+LinkedList.prototype.remove = function(position) {
+  if (position > this.length - 1) {
     throw new Error(`Out of range. This list ${this.length} length long. Max index is ${this.length - 1}.`);
   }
 
-  if (index < 0) {
+  if (position < 0) {
     throw new Error('Array index only integer');
   }
 
   let count = 0;
   let cur = this.head;
-  while(count < index - 1 && cur.next) {
+  while(count < position - 1 && cur.next) {
     cur = cur.next;
     count++;
   }
@@ -53,7 +55,7 @@ list.append(1);
 list.append(2);
 list.append(3);
 
-list.removeAt(10);
+list.remove(1);
 
 let cur = list.head;
 while(cur.next) {
